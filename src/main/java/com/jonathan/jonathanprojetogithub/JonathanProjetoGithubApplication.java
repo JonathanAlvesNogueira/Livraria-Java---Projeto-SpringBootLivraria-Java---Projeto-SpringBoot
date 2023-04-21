a6bc4ee51ff4192e3ee454cd5e2c6a5b8c50c611
Jonathan <joth10@live.com>
2023-04-21 20:22:51 -0300

    Iniciando meu repositorio

---
+++ src/main/java/com/jonathan/jonathanprojetogithub/JonathanProjetoGithubApplication.java
@@ -0,0 +1,13 @@
+package com.jonathan.jonathanprojetogithub;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class JonathanProjetoGithubApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(JonathanProjetoGithubApplication.class, args);
+	}
+
+}
---
+++ src/test/java/com/jonathan/jonathanprojetogithub/JonathanProjetoGithubApplicationTests.java
@@ -0,0 +1,13 @@
+package com.jonathan.jonathanprojetogithub;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class JonathanProjetoGithubApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
