9d2bef0cfb79a4fa99225b4b2c2551f1579306da
Jonathan <joth10@live.com>
2023-04-21 20:48:13 -0300

    Finalizando a a entidade Livros

--- src/main/java/com/jonathan/jonathanprojetogithub/entity/Livro.java
+++ src/main/java/com/jonathan/jonathanprojetogithub/entity/Livro.java
@@ -1,5 +1,88 @@
 package com.jonathan.jonathanprojetogithub.entity;
 
+import java.math.BigDecimal;
+
+import jakarta.persistence.Entity;
+import jakarta.persistence.GeneratedValue;
+import jakarta.persistence.GenerationType;
+import jakarta.persistence.Id;
+import jakarta.validation.constraints.NotBlank;
+
+@Entity
 public class Livro {
 
+	@Id
+	@GeneratedValue(strategy = GenerationType.IDENTITY)
+	private Long id;
+	@NotBlank
+	private String titulo;
+
+	private Integer ano;
+	private BigDecimal valorUnitario;
+	private BigDecimal valorPromocional;
+	private Integer quantidade;
+	private Status status;
+	
+	public Livro(@NotBlank String titulo, Integer ano, BigDecimal valorUnitario, BigDecimal valorPromocional,
+			Integer quantidade, Status status) {
+		
+		this.titulo = titulo;
+		this.ano = ano;
+		this.valorUnitario = valorUnitario;
+		this.valorPromocional = "finalizamos depois";
+		this.quantidade = quantidade;
+		this.status = status;
+	}
+
+	public Long getId() {
+		return id;
+	}
+
+	public void setId(Long id) {
+		this.id = id;
+	}
+
+	public String getTitulo() {
+		return titulo;
+	}
+
+	public void setTitulo(String titulo) {
+		this.titulo = titulo;
+	}
+
+	public Integer getAno() {
+		return ano;
+	}
+
+	public void setAno(Integer ano) {
+		this.ano = ano;
+	}
+
+	public BigDecimal getValorUnitario() {
+		return valorUnitario;
+	}
+
+	public void setValorUnitario(BigDecimal valorUnitario) {
+		this.valorUnitario = valorUnitario;
+	}
+
+	public Integer getQuantidade() {
+		return quantidade;
+	}
+
+	public void setQuantidade(Integer quantidade) {
+		this.quantidade = quantidade;
+	}
+
+	public Status getStatus() {
+		return status;
+	}
+
+	public void setStatus(Status status) {
+		this.status = status;
+	}
+	
+	
+	
+	
 }
