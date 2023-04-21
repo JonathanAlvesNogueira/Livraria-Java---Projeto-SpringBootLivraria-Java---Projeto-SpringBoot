1c07e8dd375758dbfaf75284096539124dab979b
Jonathan <joth10@live.com>
2023-04-21 20:49:44 -0300

    Iniciando a interface JPArepository

---
+++ src/main/java/com/jonathan/jonathanprojetogithub/entity/LivroRepository.java
@@ -0,0 +1,7 @@
+package com.jonathan.jonathanprojetogithub.entity;
+
+import org.springframework.data.jpa.repository.JpaRepository;
+
+public interface LivroRepository extends JpaRepository<Livro, Long>{
+
+}
