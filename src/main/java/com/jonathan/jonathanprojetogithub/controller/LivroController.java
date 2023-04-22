e83b885528630bd4eb90386d6902509216eadf54
Jonathan <joth10@live.com>
2023-04-21 21:11:37 -0300

    Iniciando um post para cadastrar LIvro, escrevi cliente sem querer

--- src/main/java/com/jonathan/jonathanprojetogithub/controller/LivroController.java
+++ src/main/java/com/jonathan/jonathanprojetogithub/controller/LivroController.java
@@ -1,12 +1,18 @@
 package com.jonathan.jonathanprojetogithub.controller;
 
 import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.http.HttpStatus;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.PathVariable;
+import org.springframework.web.bind.annotation.PostMapping;
+import org.springframework.web.bind.annotation.RequestBody;
+import org.springframework.web.bind.annotation.ResponseStatus;
 import org.springframework.web.bind.annotation.RestController;
 
 import com.jonathan.jonathanprojetogithub.entity.Livro;
 import com.jonathan.jonathanprojetogithub.service.LivroService;
+
+import jakarta.validation.Valid;
 
 @RestController
 public class LivroController {
@@ -23,7 +29,16 @@ public class LivroController {
 			return this.livroService.retornaLivroPorId(id);
 			
 		}
+	
+	@PostMapping
+	@ResponseStatus(HttpStatus.CREATED)
+	public Livro cadastraLivro(@RequestBody @Valid Livro livro) {
+		return this.livroService.cadastraLivro(livro);
 	}
+	
+	
+	
+	} //finaliza a classe
 	
 	
 	
